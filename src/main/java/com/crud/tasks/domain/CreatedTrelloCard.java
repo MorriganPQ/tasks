d8e8a458e877e973a7a9e4430bfcfda361e32ace
MorriganPQ <morrigan@fastmail.com>
2018-11-10 03:30:11 +0100

    Testing with mockito

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -20,6 +20,6 @@ public class CreatedTrelloCard {
     @JsonProperty("shortUrl")
     private String shortUrl;
 
-    @JsonProperty("badges")
-    private TrelloBadges badges;
+//    @JsonProperty("badges")
+//    private TrelloBadges badges;
 }
