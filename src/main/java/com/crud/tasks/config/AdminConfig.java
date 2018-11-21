5544a6888c0152e132b08dbb6bbaaba108df3b40
MorriganPQ <morrigan@fastmail.com>
2018-11-21 09:57:01 +0100

    Thymeleaf mail service added

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 @Getter
 @Component
 public class AdminConfig {
+    @Value("${admin.name}")
+    private String adminName;
+
     @Value("${admin.mail}")
     private String adminMail;
 }
